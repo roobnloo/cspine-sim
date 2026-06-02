f6ac21f2486d10d2fc1bedd0c0ebdfd271fcc517
roobnloo <71776622+roobnloo@users.noreply.github.com>
2026-06-02 13:29:47 -0700

    fix snr data gen

--- generate-data-varying-snr.R
+++ generate-data-varying-snr.R
@@ -4,8 +4,9 @@ suppressPackageStartupMessages(library(MASS))
 p <- 25L
 q <- 50L
 n <- 200L
-n_rep <- 5L
-c_vals <- round(sqrt(c(0.01, seq(0.025, 0.1, 0.025)) / 0.083), 2)
+n_rep <- 100L
+c_vals <- round(sqrt(c(0.01, seq(0.025, 0.125, 0.025)) / 0.083), 2)
+c_vals <- round(sqrt(0.01 / 0.083), 2)
 pd_tol <- 1e-6
 pd_fail_thresh <- 0.01
 master_seed <- 8472L
